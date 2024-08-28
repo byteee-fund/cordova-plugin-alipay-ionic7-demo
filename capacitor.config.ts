1a14df29110043d7064daed293c03cac8dd77931
Jason.z <281674669@qq.com>
2024-08-28 21:10:22 +0800

    修改参数~

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'cordova-plugin-alipay-ionic7-demo',
-  webDir: 'dist'
+  webDir: 'dist',
+  cordova: {
+    preferences: {
+      alipay_app_id: "2021003110650758",
+    }
+  },
 };
 
 export default config;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,7 +19,7 @@ const Home: React.FC = () => {
     const handlePay = () => {
         if (window.Alipay) {
             window.Alipay.payV2(
-                { orderInfo: "alipay_sdk=alipay-sdk-java-4.4.2.ALL&app_id=2021003110650758&biz_content=%7B%22body%22%3A%22%22%2C%22out_trade_no%22%3A%2210000240828090000010%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22subject%22%3A%22%E8%A1%A3%E6%9C%B5%E4%BA%91%E9%AD%94%E6%96%B9-%E5%BA%94%E7%94%A8%E5%85%85%E5%80%BC%22%2C%22timeout_express%22%3A%2230m%22%2C%22total_amount%22%3A%220.01%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=https%3A%2F%2Fpay-cube.test.ydoyun.com%2Fali%2Fnotify&sign=jP606BIM2FoCB12QUQ0JgejtfINdebGnOqmDP9T7YmEOAiN4%2B8UV7zK1gXip%2Fch31EYfJEcvRDB2FG%2BGNzDT2FGq368YZHW2VrLXhARuLyJ37bXtL%2BQMCZdDNzkEsTIZZyAR4upI9zUp%2BW2L72k1fiGaY5angMa%2FXlf1jldh8XbVoqRKUfxmIcs4Yb3r%2Bgl7VEuB%2FDrA5Rqy8c%2BM60XGMxbHbYUgcAIvL7azQSQBSrx8%2FcinmLVAe%2FYQ5D3uKjQO8nMOPRHih9RY6%2FLkF7jH4hBHZkU3ZLcxJBqB6UAccIPFrGiRoWtZqIcaoRmuFPJqGhpaW8T3llYtBtI94yiD5Q%3D%3D&sign_type=RSA2&timestamp=2024-08-28+09%3A46%3A10&version=1.0" },
+                { orderInfo: "alipay_sdk=alipay-sdk-java-4.4.2.ALL&app_id=2021003110650758&biz_content=%7B%22body%22%3A%22%22%2C%22out_trade_no%22%3A%2210000240828160000001%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22subject%22%3A%22%E8%A1%A3%E6%9C%B5%E4%BA%91%E9%AD%94%E6%96%B9-%E5%BA%94%E7%94%A8%E5%85%85%E5%80%BC%22%2C%22timeout_express%22%3A%2230m%22%2C%22total_amount%22%3A%220.01%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=https%3A%2F%2Fpay-cube.test.ydoyun.com%2Fali%2Fnotify&sign=OsUs4buxkj9PUaWCq69puKotaGFi0Ao4tiMFgz1aMoSP2kpuBJ16jGvpHIP8qM4kXeTzgb48fkHyOPlkE5GpC9UcO2Ie7evMMOmyPywyyw6I5Q3tOUVPy%2FF1F3xlFX0081OQuTqOpZ8y4sAwlTLBmfqGnFzqwYmtP4j1G%2BoJmxaHrsIIAuxrP2Lkqq0peSUXdm1eLjNN69%2BklexZaYG%2BofTqZb8sKBGPltDNtCkmdHC7p%2BchR%2BAG1jM8Gct4Pm9ryu5Y2NN1Kg%2FW97Y8BFWVr%2BqjATTdHl95Mq53b7JrmReh8sM8%2FGkdUD46wU0y7MHnkoVGw406L9O93%2FVe2st8KQ%3D%3D&sign_type=RSA2&timestamp=2024-08-28+16%3A43%3A18&version=1.0" },
                 function (response) {
                     alert(JSON.stringify(response));
                 },
@@ -32,19 +32,24 @@ const Home: React.FC = () => {
 
     return (
         <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Cordova Plugin Alipay Demo</IonTitle>
+                </IonToolbar>
+            </IonHeader>
             <IonContent fullscreen>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Cordova Plugin Alipay Demo</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
+                {/*<IonHeader collapse="condense">*/}
+                {/*    <IonToolbar>*/}
+                {/*        <IonTitle size="large">Cordova Plugin Alipay Demo</IonTitle>*/}
+                {/*    </IonToolbar>*/}
+                {/*</IonHeader>*/}
                 <div className="container">
                     <IonList lines="full">
                         <IonItem onClick={handleAuth}>
-                            auth
+                            authV2
                         </IonItem>
                         <IonItem onClick={handlePay}>
-                            pay
+                            payV2
                         </IonItem>
                     </IonList>
                 </div>
